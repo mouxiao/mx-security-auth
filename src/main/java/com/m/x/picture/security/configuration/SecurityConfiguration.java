7d4689add6a0437d7d485253d0189f606c0676dd
mouxiao1014@gmail.com <muxiao911014>
2019-08-07 23:25:54 +0800

    init

---
+++ src/main/java/com/m/x/picture/security/configuration/SecurityConfiguration.java
@@ -0,0 +1,23 @@
+package com.m.x.picture.security.configuration;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author xiao.mou
+ * @remark
+ * @date 2019/7/10 0010 22:19
+ */
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+
+  /**
+   * 基于表单认证
+   * @param http
+   * @throws Exception
+   */
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    super.configure(http);
+  }
+}
